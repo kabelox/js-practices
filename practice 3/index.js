26c62a029667731fc16f8425bb770a587b5e2338
Owethu99Twala <owethutwala99@gmail.com>
2020-08-11 11:05:54 +0200

    js practice3 version 2

--- practice 3/index.js
+++ practice 3/index.js
@@ -1,8 +1,23 @@
-function stringReverse(str){
-let reverseStr = str.split("");
- let result = reverseStr.reverse().join("")
+// function stringReverse(str){
+// let reverseStr = str.split("");
+//  let result = reverseStr.reverse().join("")
+// return result;
+
+// }
+
+// console.log(stringReverse("hello"));
+
+
+// version 2 
+
+function funcReverse(str){
+let result = "";
+
+for(let i = str.length -1 ; i >= 0;i--){
+result += str[i];
+}
 return result;
 
 }
 
-console.log(stringReverse("hello"));
+console.log(funcReverse("hello"));
