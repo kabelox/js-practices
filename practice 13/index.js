e7aebb7561eb5dbdcf4c4cbd37806f379d99b5fb
Owethu99Twala <owethutwala99@gmail.com>
2020-08-18 15:31:56 +0200

    the slicer (return a spliced mutated array)

---
+++ practice 13/index.js
@@ -0,0 +1,11 @@
+function slacer(arr,num){
+    let result = [];
+    while(arr.length>0){
+        res = arr.splice(0,num);
+       
+    }
+    return res;
+    
+    
+            }
+    console.log(slacer([2,4,5],2));
