351692ab77b68dc5ef323669d7d96a304459147d
Owethu99Twala <owethutwala99@gmail.com>
2020-08-26 10:25:07 +0200

    the battle ship game!

--- project 16/index.js
+++ project 16/index.js
@@ -1,16 +1,34 @@
-function wordLooker(arr1,arr2){
-    let result = [];
-    for(let i = 0;i<arr1.length;i++){
-        if(arr2.indexOf(arr1[i]) === -1)
-        result.push(arr1[i]);
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+let guess=0;
+let hits = 0;
+let guesses = 0;
+let isunk = false
+
+while(isunk == false){
+    guess = prompt("ready fire go!(0-6)");
+        if(guess<0 || guess>6){
+        alert("please enter valid!")
     }
+    else{
+        guesses++;
+    }  
+
+        if(guess == location1 || guess == location2 || guess == location3){
+            hits++;
+            alert("HIt")
+        }
+        else{
+            alert("Missed!")
+        }
 
 
-    for(let j = 0; j<arr2.length;j++){
-        if(arr1.indexOf(arr2[j]) === -1)
-        result.push(arr2[j]);
+        if(hits == 3){
+            isunk = true;
+            alert("you've finished me!")
+        }
 
-    }
-return result;
 }
-console.log(wordLooker([1,2,3,5],[3,4,6]));
+let stats = "you've took " +guesses+ "to sink the enemy's ship and your accuracy was " +(3/guesses);
+alert(stats);
