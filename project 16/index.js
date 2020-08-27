969fe85bbc0252362d727380234f4eb2c70b9fba
Owethu99Twala <owethutwala99@gmail.com>
2020-08-27 10:05:40 +0200

    the battle ship better  game!

--- project 16/index.js
+++ project 16/index.js
@@ -1,12 +1,13 @@
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+let random = Math.floor(Math.random()*5);
+let location1 = random;
+let location2 = location1 +1;
+let location3 = location2 +1;
 let guess=0;
 let hits = 0;
 let guesses = 0;
 let isunk = false
 
-while(isunk == false){
+while(isunk==false){
     guess = prompt("ready fire go!(0-6)");
         if(guess<0 || guess>6){
         alert("please enter valid!")
